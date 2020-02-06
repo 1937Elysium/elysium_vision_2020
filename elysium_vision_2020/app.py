f4e2facfc91b1ef1579c43a584c601bcba13f39a
1937Elysium <frcelysium1937@gmail.com>
2020-02-06 23:41:42 +0200

    changed: import to absolute import

--- elysium_vision_2020/app.py
+++ elysium_vision_2020/app.py
@@ -1,4 +1,4 @@
-from .visions.vision_controller import vision_controller
+from elysium_vision_2020.visions.vision_controller import vision_controller
 
 if __name__ == '__main__':
     for directions, contours, filtered_image in vision_controller.start():
