c0c91385d3e6b8f61c3660e2fb2d33e5c4a217ca
1937Elysium <frcelysium1937@gmail.com>
2020-02-06 23:53:21 +0200

    Added: second camera port opened when using windows

--- elysium_vision_2020/connections.py
+++ elysium_vision_2020/connections.py
@@ -1,6 +1,8 @@
 import cv2
+import platform
 
 from elysium_vision_2020.configurations import *
 
 ROBOT_NETWORK_TABLES_CONNECTION = ovl.NetworkTablesConnection(TEAM_NUMBER, table_name=NETWORK_TABLES_TABLE)
-CAMERA = cv2.VideoCupture(0)
+CAMERA_PORT = 1 if platform.system() == "Windows" else 0
+CAMERA = cv2.VideoCapture(CAMERA_PORT)
