db63cf3dece56625ea7cf1254c8e62f913e93733
1937Elysium <frcelysium1937@gmail.com>
2020-02-06 23:44:03 +0200

    changed: import to absolute import

--- elysium_vision_2020/connections.py
+++ elysium_vision_2020/connections.py
@@ -1,6 +1,6 @@
 import cv2
 
-from .configurations import *
+from elysium_vision_2020.configurations import *
 
 ROBOT_NETWORK_TABLES_CONNECTION = ovl.NetworkTablesConnection(TEAM_NUMBER, table_name=NETWORK_TABLES_TABLE)
 CAMERA = cv2.VideoCupture(0)
