9d5a99c11f3fd2a27d39a7e8bb82bdf31ff4f4d6
1937Elysium <frcelysium1937@gmail.com>
2020-02-06 19:16:47 +0200

    Extracted: vision_controller to app.py

---
+++ elysium_vision_2020/app.py
@@ -0,0 +1,5 @@
+from .visions.vision_controller import vision_controller
+
+if __name__ == '__main__':
+    for directions, contours, filtered_image in vision_controller.start():
+        pass
--- elysium_vision_2020/visions/vision_controller.py
+++ elysium_vision_2020/visions/vision_controller.py
@@ -6,9 +6,3 @@ visions = []
 vision_controller = ovl.MultiVision(update_connection=ROBOT_NETWORK_TABLES_CONNECTION,
                                     update_location=CONTROLLER_UPDATE_LOCATION,
                                     vision_list=visions)
-
-if __name__ == '__main__':
-    for directions, contours, filtered_image in vision_controller.start():
-        pass
-
-
