41da7e67b8b57d92f7c29fa2c0820d2800129b09
1937Elysium <frcelysium1937@gmail.com>
2020-02-15 00:40:49 +0200

    Added: Added sending of directions to the network location

--- elysium_vision_2020/app.py
+++ elysium_vision_2020/app.py
@@ -1,5 +1,7 @@
 from elysium_vision_2020.visions.vision_controller import vision_controller
+from elysium_vision_2020.configurations import VISION_TO_NETWORK_LOCATION
 
 if __name__ == '__main__':
     for directions, contours, filtered_image in vision_controller.start():
-        pass
+        network_location = VISION_TO_NETWORK_LOCATION[vision_controller]
+        vision_controller.current.send_to_location(directions, network_location=network_location)
