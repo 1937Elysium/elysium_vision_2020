e74710129d02286d96f6c37ad67aa992031b205a
1937Elysium <frcelysium1937@gmail.com>
2020-02-14 23:20:24 +0200

    Added: VisionController vision mapping as dictionary

--- elysium_vision_2020/configurations.py
+++ elysium_vision_2020/configurations.py
@@ -7,6 +7,10 @@ UPDATE_KEY = "current_vision"
 TARGET_X_KEY = "target_x"
 TARGET_AMOUNT_KEY = "target_amount"
 
+FEEDER_VISION_NAME = "feeder"
+ONE_POWER_CELL_NAME = "ball_vision"
+TWO_POWER_CELL_NAME = "two_balls"
+
 
 CONTROLLER_UPDATE_LOCATION = ovl.NetworkLocation(table_key=UPDATE_KEY)
 X_DIRECTION_LOCATION = ovl.NetworkLocation(table_key=TARGET_X_KEY)
--- elysium_vision_2020/visions/vision_controller.py
+++ elysium_vision_2020/visions/vision_controller.py
@@ -1,7 +1,14 @@
 from elysium_vision_2020.connections import *
+from elysium_vision_2020.visions.power_cell_visions.power_cell import one_power_cell
+from elysium_vision_2020.visions.power_cell_visions.feeder_vision import feeder_vision
+from elysium_vision_2020.visions.power_cell_visions.two_power_cells import two_power_cell
+from elysium_vision_2020.configurations import *
 
-visions = []
+visions = {FEEDER_VISION_NAME: feeder_vision,
+           ONE_POWER_CELL_NAME: one_power_cell,
+           TWO_POWER_CELL_NAME: two_power_cell}
 
 vision_controller = ovl.MultiVision(update_connection=ROBOT_NETWORK_TABLES_CONNECTION,
                                     update_location=CONTROLLER_UPDATE_LOCATION,
-                                    vision_list=visions)
+                                    vision_list=visions,
+                                    default_vision="feeder")
